be658f0476c2fa2513bc7ff92e5e94078c0ca964
DevBigEazi <adesholatajudeen1@gmail.com>
2023-10-19 08:23:52 +0100

    Cohort 4.0 schema created

---
+++ schemas/trainingC4.js
@@ -0,0 +1,78 @@
+export default {
+    name: 'trainingc4',
+    type: 'document',
+    title: 'Join TrainingC4',
+    fields: [
+      {
+        name: 'firstname',
+        type: 'string',
+        title: 'First Name',
+      },
+      {
+        name: 'lastname',
+        type: 'string',
+        title: 'Last Name',
+      },
+      {
+        name: 'stateOfOrigin',
+        type: 'string',
+        title: 'State Of Origin',
+      },
+      {
+        name: 'gender',
+        type: 'string',
+        title: 'Gender',
+      },
+      {
+        name: 'dob',
+        type: 'string',
+        title: 'DOB',
+      },
+      {
+        name: 'phoneNumber',
+        type: 'string',
+        title: 'Phone Number',
+      },
+      {
+        name: 'acadQual',
+        type: 'string',
+        title: 'Academic Qualification',
+      },
+      {
+        name: 'email',
+        type: 'string',
+        title: 'Email Address',
+      },
+      {
+        name: 'exp',
+        type: 'string',
+        title: 'Coding Experience',
+      },
+      {
+        name: 'courseSelected',
+        type: 'string',
+        title: 'Course Selected',
+      },
+      {
+        name: 'html',
+        type: 'string',
+        title: 'HTML',
+      },
+      {
+        name: 'css',
+        type: 'string',
+        title: 'CSS',
+      },
+      {
+        name: 'javascript',
+        type: 'string',
+        title: 'Javascript',
+      },
+      {
+        name: 'noneOfTheAbove',
+        type: 'string',
+        title: 'None Of The Above',
+      },
+    ],
+  }
+  
